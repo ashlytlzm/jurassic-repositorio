a3b9c443ee68350ad9c08e5006a3ab9b13f337dc
ashly <ashly@10.14.38.249>
2025-05-09 12:21:52 -0500

    agregue los metodos y atributos a la clase de feeding

--- src/Feedings/Feeding.java
+++ src/Feedings/Feeding.java
@@ -3,5 +3,44 @@ package Feedings;
 
 
 public class Feeding {
+    private String typeOfFood;
+    private int dailyAmount;
+    private int frecuency;
+
+     public Feeding() {
+        
+    }
+    public Feeding(String typeOfFood, int dailyAmount, int frecuency) {
+        this.typeOfFood = typeOfFood;
+        this.dailyAmount = dailyAmount;
+        this.frecuency = frecuency;
+    }
+
+    public String getTypeOfFood() {
+        return typeOfFood;
+    }
+
+    public int getDailyAmount() {
+        return dailyAmount;
+    }
+
+    public int getFrecuency() {
+        return frecuency;
+    }
+
+    public void setTypeOfFood(String typeOfFood) {
+        this.typeOfFood = typeOfFood;
+    }
+
+    public void setDailyAmount(int dailyAmount) {
+        this.dailyAmount = dailyAmount;
+    }
+
+    public void setFrecuency(int frecuency) {
+        this.frecuency = frecuency;
+    }
+    
+    
+    
     
 }
